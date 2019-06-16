35ad8d789a47357676b3e639a8748088740ca186
projectImplementer <bogdanm.demetrescu@gmail.com>
2019-06-16 21:19:22 +0300

    first changes

--- app/src/main/java/com/example/clickarithmetic/MainActivity.java
+++ app/src/main/java/com/example/clickarithmetic/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.clickarithmetic;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +13,16 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void onClick(View v){
+        goToSecondPage();
+
+    }
+
+
+    public void goToSecondPage(){
+        Intent openSecondPage = new Intent(this, SecondActivity.class );
+        startActivity(openSecondPage);
+    }
+
 }
