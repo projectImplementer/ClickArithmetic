9979dc6b29d9b7fe4af167bf7fccf02448c37903
victor94 <vv_a15@yahoo.com>
2019-06-17 17:15:24 +0300

    Added addition

--- app/src/main/java/com/example/clickarithmetic/MainActivity.java
+++ app/src/main/java/com/example/clickarithmetic/MainActivity.java
@@ -2,11 +2,13 @@ package com.example.clickarithmetic;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.NumberPicker;
+import android.widget.Toast;
 
 import java.util.Scanner;
 
@@ -14,6 +16,8 @@ public class MainActivity extends AppCompatActivity {
 
     private EditText number1;
     private EditText number2;
+    double result;
+    String strResult;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,34 +25,30 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    public void onClick(View v)
-    {
-        setContentView(R.layout.activity_main);
+    @SuppressLint("DefaultLocale")
+    public void ShowData_Click(View v) {
+        number1 = findViewById(R.id.nr1);
+        number2 = findViewById(R.id.nr2);
 
-        number1 = findViewById(R.id.et_input1);
-        number2 = findViewById(R.id.et_input2);
+        result = Double.valueOf(number1.getText().toString()) + Double.valueOf(number2.getText().toString());
+
+        if (result == (int) result)
+            strResult = String.format("%d", (int) result);
+        else
+            strResult = String.format("%s", result);
+
+        Toast.makeText(getApplicationContext(), strResult,
+                Toast.LENGTH_LONG).show();
 
         goToSecondPage();
     }
 
 //TODO: pass parameter to second page
-    public void goToSecondPage(){
+    public void goToSecondPage()
+    {
         Intent openSecondPage = new Intent(this, SecondActivity.class);
         startActivity(openSecondPage);
-    }
 
-    Integer num1;
-    Integer num2;
-    char operator;
-    double answer = 0.0;
-
-    Scanner scanObject = new Scanner(System.in);  // create Scanner type object.
-    public void numberOneInput() {
-
-       // Scanner scanObject = new Scanner(System.in);  // create Scanner type object.
-
-        //System.out.println("The value of x is: ");
-        num1 = scanObject.nextInt(); // the terminal waits for user input for num1.
     }
 
 
