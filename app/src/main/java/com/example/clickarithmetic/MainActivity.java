a28aba6d9a9632c50391148a087637e6fc87c109
victor94 <vv_a15@yahoo.com>
2019-06-17 16:32:44 +0300

    Added onclick - not working

--- app/src/main/java/com/example/clickarithmetic/MainActivity.java
+++ app/src/main/java/com/example/clickarithmetic/MainActivity.java
@@ -5,8 +5,13 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.NumberPicker;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText number1;
+    private EditText number2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,11 +19,17 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    public void onClick(View v){
+    public void onClick(View v)
+    {
+        setContentView(R.layout.activity_main);
+
+        number1 = findViewById(R.id.et_input1);
+        number2 = findViewById(R.id.et_input2);
+
         goToSecondPage();
     }
 
-//TODO: pass parameter to second page somePage
+//TODO: pass parameter to second page
     public void goToSecondPage(){
         Intent openSecondPage = new Intent(this, SecondActivity.class);
         startActivity(openSecondPage);
